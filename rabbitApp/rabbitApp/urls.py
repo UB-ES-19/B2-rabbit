b96fbf52084ed4d982fc7f1913f2026333a94027
rogerravent <rogerravent@gmail.com>
2019-09-26 18:56:56 +0200

    merge with dev

--- rabbitApp/rabbitApp/urls.py
+++ rabbitApp/rabbitApp/urls.py
@@ -24,5 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
     path('register/', views.register)
-] + static(settings.STATIC_URL)
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
