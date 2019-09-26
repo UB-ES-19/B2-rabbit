752e847c0a19a97cd7458e89df3f09857255b3fa
rogerravent <rogerravent@gmail.com>
2019-09-26 11:09:42 +0200

    Register view implemented.

--- rabbitApp/rabbit/views.py
+++ rabbitApp/rabbit/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
 def index(request):
-    return render(request, '')
+    return render(request, 'base.html')
+
+
+def register(request):
+    context = {
+    }
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+        else:
+            context["errors"] = form.errors
+
+    return redirect(index, context)
