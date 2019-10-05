b651f628b7a6ca3866ef2a32611332d933615ce0
Alejandro Cano <alex.cano1994@gmail.com>
2019-10-05 18:37:31 +0200

    US5-1 Creacion del modelo Post

--- rabbitApp/rabbit/models.py
+++ rabbitApp/rabbit/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.TextField(max_length=255)
+    description = models.TextField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
--- rabbitApp/rabbitApp/urls.py
+++ rabbitApp/rabbitApp/urls.py
@@ -22,7 +22,7 @@ from rabbitApp import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
+    path('', views.index, name='index'),
     path('register', views.register, name='register'),
     path('login', views.login_user, name='login'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
