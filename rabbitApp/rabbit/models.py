27bd8253357498bb577c3e138d8fb0090d958357
rogerravent <rogerravent@gmail.com>
2019-10-07 14:56:01 +0200

    Creación del modelo Padre Post y sus hijos. Description, image y link.

--- rabbitApp/rabbit/models.py
+++ rabbitApp/rabbit/models.py
@@ -6,6 +6,25 @@ from django.db import models
 class Post(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255)
-    description = models.TextField(max_length=255, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     creation_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+def get_image_filename(instance, filename):
+    post_id = instance.post.id
+    return "images/%s" % post_id + "/" + filename
+
+
+class DescriptionPost(Post):
+    description = models.TextField(max_length=255, blank=True)
+
+
+class ImgPost(Post):
+    img = models.ImageField(upload_to=get_image_filename)
+
+
+class LinkPost(Post):
+    link = models.URLField()
