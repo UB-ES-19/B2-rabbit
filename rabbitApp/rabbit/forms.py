a92e99fce0d95b5b49301820747e6a0a0f767b93
rogerravent <rogerravent@gmail.com>
2019-10-27 12:47:42 +0100

    Endpoint de crear un warren init.

--- rabbitApp/rabbit/forms.py
+++ rabbitApp/rabbit/forms.py
@@ -30,3 +30,12 @@ class LinkPostForm(ModelForm):
             'title': forms.TextInput(attrs={'placeholder': 'Title'}),
             'link': forms.TextInput(attrs={'placeholder': 'Write ur link', 'class': 'form-control'}),
         }
+
+class WarrenForm(ModelForm):
+    class Meta:
+        model = Warren
+        fields = ['name', 'description', 'profile_img', 'landscape_img']
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'Name'}),
+            'description': forms.Textarea(attrs={'placeholder': 'Write a little description'}),
+        }
