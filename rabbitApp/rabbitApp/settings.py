22d09bb1d6fc2b020b90be33bc00160bb5ea019d
rogerravent <rogerravent@gmail.com>
2019-10-29 12:29:59 +0100

    Añadida contraseña para la base de datos en el settings y poniendo bien el nombre del directorio.

--- rabbitApp/rabbitApp/settings.py
+++ rabbitApp/rabbitApp/settings.py
@@ -79,6 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
+	    'PASSWORD': 'changeme',
         'HOST': 'db',
         'PORT': 5432,
     }
