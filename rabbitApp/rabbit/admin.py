bb14b14c4e0cf16a1c332c1a17e4bb792ee4594a
rogerravent <rogerravent@gmail.com>
2019-11-06 20:31:32 +0100

    Modelo de datos de Follower / Following creado.

--- rabbitApp/rabbit/admin.py
+++ rabbitApp/rabbit/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Post, Warren
+from .models import Post, Warren, Follower
+
 # Register your models here.
 
 
 admin.site.register(Post)
 admin.site.register(Warren)
+admin.site.register(Follower)
