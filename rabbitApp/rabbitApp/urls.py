8c523908409b05199222c9e47fe270e9cd9067ed
XavierMacias <xmaciaxi7@alumnes.ub.edu>
2019-11-17 17:07:33 +0100

    eliminar posts

--- rabbitApp/rabbitApp/urls.py
+++ rabbitApp/rabbitApp/urls.py
@@ -35,6 +35,7 @@ urlpatterns = [
     path('w/<str:name>', views.warren, name='warren'),
     path('r/<str:name>/', views.profile, name='profile'),
     path('r/<str:name>', views.profile, name='profile'),
+    path('delete/<str:id>',views.delete, name='delete')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
