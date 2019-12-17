6adc2662e6bf0654b4866f7e9bc88d7725d02acd
rogerravent <rogerravent@gmail.com>
2019-12-17 12:00:36 +0100

    project root

--- rabbitApp/manage.py
+++ rabbitApp/manage.py
@@ -6,7 +6,6 @@ import sys
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rabbitApp.settings')
-    print (os.path.dirname(os.path.abspath(__file__)))
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- rabbitApp/rabbitApp/settings.py
+++ rabbitApp/rabbitApp/settings.py
@@ -19,6 +19,7 @@ import django_heroku
 import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
